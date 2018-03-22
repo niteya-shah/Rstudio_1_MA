bdbf410be8ba8e7f593fe823713719233ae41491
niteya-shah <30979819+niteya-shah@users.noreply.github.com>
2018-03-22 15:10:06 +0530

    3

--- first.r
+++ first.r
@@ -1,3 +1,16 @@
 #my first R file
-women
-version
+#4 - like below for creating a bookmark
+#vectors----
+v1=c('A','B','C')
+v2=c(1,2,3)
+v3=c(TRUE,TRUE,FALSE)
+v3a=c(T,F,T)
+v4=1:10
+(v5=seq(1,10,2))   #( ) bracket outside for printing
+v4;v5;v1 #printing multiple vectors
+v4[c(2,3,5)] #selective printing
+v4[2:5]
+v4[-(2:5)]
+v4[v4>5]
+v1[v1>='B']
+v4[v1 %in% c('A','C')]
